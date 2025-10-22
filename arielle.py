38341ba59022989b8effb09660a40725c55ebeae
demurean <arielle.felicia@gmail.com>
2025-10-21 17:02:01 -0700

    something

--- arielle.py
+++ arielle.py
@@ -1,2 +1,19 @@
 # Exploratory Data Analysis
 # working on the distributions of key features as histograms
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('student_performance_dataset.csv')
+
+gender = df['Gender']
+
+age = df['Age']
+
+department = df['Department']
+
+attendance = df['attendance']
+
+plt.hist(gender, bins=30)
+
+
