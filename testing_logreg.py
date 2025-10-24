f60793102c5f0fca9f4e029b307f8e5b83a98710
demurean <arielle.felicia@gmail.com>
2025-10-23 17:59:14 -0700

    logreg on credit card fraud dataset

--- testing_logreg.py
+++ testing_logreg.py
@@ -7,25 +7,26 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 # Load dataset
-df = pd.read_csv('student_performance_dataset.csv')
+df = pd.read_csv('fraudTest.csv')
 
 # Encode Grade (A,B,C,D,F) → numeric labels (e.g., A=0, B=1, etc.)
-le = LabelEncoder()
-df['Grade_encoded'] = le.fit_transform(df['Grade'])
+# le = LabelEncoder()
+# df['Grade_encoded'] = le.fit_transform(df['Grade'])
 # print(dict(zip(le.classes_, le.transform(le.classes_)))) 
 
-X = df[['Age', 'Attendance (%)', 'Study_Hours_per_Week', 'Stress_Level (1-10)', 'Sleep_Hours_per_Night']]
-y = df['Grade_encoded']
+categorical_X = df[['category', 'gender', 'job']]
+numeric_X= df[['amt', 'lat', 'long', 'merch_lat', 'merch_long']]
+y = df['is_fraud']
 
 # Standardize numeric features for interpretability
 scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+X_scaled = scaler.fit_transform(numeric_X)
 
 # Train multinomial logistic regression
 model = LogisticRegression(multi_class='multinomial', solver='lbfgs', max_iter=1000)
 model.fit(X_scaled, y)
 
-coefficients = pd.DataFrame(model.coef_, columns=X.columns, index=le.classes_)
+coefficients = pd.DataFrame(model.coef_, columns=numeric_X.columns)
 print("Feature coefficients by grade level:\n")
 print(coefficients)
 
